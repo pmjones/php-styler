d628282c7451b408ae91d5c57c8130eef6b9334e
Paul M. Jones <pmjones@pmjones.io>
2023-08-04 10:46:44 -0500

    allow for caching time of last run

--- src/Command/Apply.php
+++ src/Command/Apply.php
@@ -20,27 +20,42 @@ class Apply
 
     public function __invoke(string $configFile) : int
     {
-        $time = time();
+        $start = microtime(true);
+        $count = 0;
+        $this->printer = new Printer();
 
         // load config
-        $config = $this->loadConfig($configFile);
+        $config = $this->load($configFile);
+        $cache = ['time' => filemtime($configFile)];
+
+        // load cache
+        $cacheFile = $config['cache'] ?? false;
+        unset($config['cache']);
+
+        if ($cacheFile && file_exists($cacheFile)) {
+            $cache = $this->load($cacheFile);
+        } else {
+            $cache = ['time' => 0];
+        }
+
+        // get files to style
         $files = $config['files'] ?? [];
         unset($config['files']);
 
-        // set up services
-        $this->printer = new Printer();
-        $this->styler = new Styler(...$config);
-        $count = 0;
+        // get the configured styler object
+        $styler = $config['styler'] ?? null;
+        unset($config['styler']);
+        $this->styler = $styler ?? new Styler(...$config);
 
         // apply to files
         foreach ($files as $file) {
-            $count ++;
             $file = (string) $file;
 
-            if (is_dir($file)) {
-                echo $file . PHP_EOL;
+            if (is_dir($file) || filemtime($file) < $cache['time']) {
                 continue;
             }
+
+            $count ++;
 
             if (! $this->lint($file)) {
                 return 1;
@@ -49,18 +64,28 @@ class Apply
             $this->replace($file);
         }
 
-        echo PHP_EOL
-            . "{$count} files styled in "
-            . (time() - $time)
+        if ($cacheFile) {
+            echo "Saving {$cacheFile}" . PHP_EOL;
+            $data = '<?php return '
+                . var_export(['time' => time()], true)
+                . ';'
+                . PHP_EOL
+            ;
+            file_put_contents($cacheFile, $data);
+        }
+
+        echo "Styled {$count} files in "
+            . number_format(microtime(true) - $start, 3)
             . ' seconds.'
             . PHP_EOL
         ;
         return 0;
     }
 
-    protected function loadConfig(string $configFile) : array
+    protected function load(string $file) : array
     {
-        return require $configFile;
+        echo "Loading " . $file . PHP_EOL;
+        return require $file;
     }
 
     protected function lint(string $file) : bool
