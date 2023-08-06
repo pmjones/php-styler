639124aa64ee620adb0c61db6ff32d3a26d1c727
Paul M. Jones <pmjones@pmjones.io>
2023-08-06 17:10:23 -0500

    fix bin autoload

--- bin/php-styler.php
+++ bin/php-styler.php
@@ -1,7 +1,27 @@
+#!/usr/bin/env php
 <?php
 use AutoShell\Console;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+$autoload = null;
+
+$autoloadFiles = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php'
+];
+
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        $autoload = $autoloadFile;
+        break;
+    }
+}
+
+if (! $autoload) {
+    echo "Autoload file not found; try 'composer dump-autoload' first." . PHP_EOL;
+    exit(1);
+}
+
+require $autoload;
 
 $console = Console::new(
     namespace: 'PhpStyler\Command',
