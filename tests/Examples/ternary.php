a894cd21c790b8c08df4f2fdf34c4c5bf06ac704
Paul M. Jones <pmjones@pmjones.io>
2023-08-10 07:08:19 -0500

    move ternary split precedence

--- tests/Examples/ternary.php
+++ tests/Examples/ternary.php
@@ -1,3 +1,23 @@
 <?php
 $a ?: $b;
 $a ? $b : $c;
+
+// ternary vs member operator
+$veryLongVariableName = $veryLongVariableName2 instanceof VeryLongClassName
+    ? clone $veryLongVariableName2
+    : $veryLongVariableName2->getResponse();
+
+// ternary vs member operator
+$foo = str_starts_with($veryLongVariableName, '.')
+    ? $this->relative($name, $this->dirname(end($this->names)))
+    : $veryLongVariableName;
+
+// ternary vs arguments
+$veryLongVariableName['selected'] = is_array($selected)
+    ? in_array($veryLongVariableName['value'], $selected)
+    : $veryLongVariableName['value'] == $selected;
+
+// ternary vs arguments
+$veryLongVariableName = is_array($veryLongVariableName)
+    ? $veryLongVariableName[key($veryLongVariableName)]
+    : $veryLongVariableName;
