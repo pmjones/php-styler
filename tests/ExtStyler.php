dd828c1116701e32bf9a57cd68f745f0c0478f6b
Paul M. Jones <pmjones@pmjones.io>
2023-09-22 15:21:05 -0500

    use new styler methods

--- tests/ExtStyler.php
+++ tests/ExtStyler.php
@@ -7,16 +7,9 @@ use PhpStyler\Printable as P;
 
 class ExtStyler extends Styler
 {
-    protected function sFunctionBody(P\Body $p) : void
+    protected function functionBodyClipWhen() : callable
     {
-        $this->newline();
-        $this->clip(
-            when: fn (string $lastLine) : bool => str_starts_with(trim($lastLine), ')'),
-            append: ' ',
-        );
-        $this->line[] = '{';
-        $this->newline();
-        $this->indent();
+        return fn (string $lastLine) : bool => str_starts_with(trim($lastLine), ')');
     }
 
     protected function sReturnType(P\ReturnType $p) : void
