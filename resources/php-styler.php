0c8d0931cf9dac28a0b9dbc583f016d1bf71e5c8
Paul M. Jones <pmjones@pmjones.io>
2026-03-25 21:58:12 -0500

    update default config

--- resources/php-styler.php
+++ resources/php-styler.php
@@ -1,8 +1,15 @@
 <?php
 use PhpStyler\Config;
+use PhpStyler\Format\DeclarationFormat;
 use PhpStyler\Files;
 
 return new Config(
     files: new Files(__DIR__ . '/src'),
     cache: __DIR__ . '/.php-styler.cache',
+    format: new DeclarationFormat(
+        lineLen: 84,
+        indentLen: 4,
+        indentTab: false,
+        eol: "\n",
+    ),
 );
